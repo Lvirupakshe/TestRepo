9e6f8d8a595656a14247aae922b81f4082d2c14c
Laxman Virupakshe <avirupakshe@gmail.com>
2022-06-20 08:36:44 +0530

    new

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+
+public class Test {
+	
+	
+	static 
+	{
+	System.out.println("Static braces");	
+	}
+	
+public static class Test1{
+	
+	Test1(){System.out.println("Constructor");};
+	public void display() {System.out.println("Parent Display");}
+}
+
+public static class Test2 extends Test1{
+	Test2(){
+		super();
+	};
+	public void display() {
+		
+		System.out.println("Child Display");
+		super.display();}
+}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Test2 t= new Test2();
+	t.display();
+}
+
+}
